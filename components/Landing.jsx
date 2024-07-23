ca2e3c3d201fcf2a75ec08ef7da921dfc83c0f97
Om Sarraf <itsomsarraf+github@gmail.com>
2024-07-24 01:34:21 +0530

    lucide icons

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -1,5 +1,6 @@
-import DynamicGridBackground from './DynamicGridBackground';
+import DynamicGridBackground from '@/components/DynamicGridBackground';
 import { APP_NAME } from '@/lib/constants';
+import { MoveRight } from 'lucide-react';
 export default function Landing() {
 	return (
 		<DynamicGridBackground>
@@ -16,16 +17,9 @@ export default function Landing() {
 							href='#'
 							className='inline-flex items-center px-6 py-3 text-lg font-medium text-white bg-orange-500 rounded-md hover:bg-orange-800'>
 							Join Waitlist
-							<svg
-								className='w-5 h-5 ml-2 -mr-1'
-								fill='currentColor'
-								viewBox='0 0 20 20'
-								xmlns='http://www.w3.org/2000/svg'>
-								<path
-									fillRule='evenodd'
-									d='M10.293 15.707a1 1 0 010-1.414L13.586 11H3a1 1 0 110-2h10.586l-3.293-3.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z'
-									clipRule='evenodd'></path>
-							</svg>
+							<div className='ml-2'>
+								<MoveRight />
+							</div>
 						</a>
 					</div>
 				</div>
